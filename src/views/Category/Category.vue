b9c3a9335b1b0a7fabdf71f1ff2b057d7c8ff217
venb <1136500285@qq.com>
2020-04-21 17:47:04 +0800

    '增加分类页面'

--- src/views/Category/Category.vue
+++ src/views/Category/Category.vue
@@ -1,13 +1,65 @@
 <template>
-  <h1>分类</h1>
+  <div>
+    <nav-bar class="nav"><div slot="center">购物街</div> </nav-bar>
+    <el-container>
+      <el-aside width="20%">
+        <el-menu
+              :default-active="activeIndex"
+              background-color="#666"
+              text-color="#fff"
+              active-text-color="#ffd04b"
+        >
+        <el-menu-item index="1">上衣</el-menu-item>
+        <el-menu-item index="2">裤子</el-menu-item>
+        <el-menu-item index="3">内衣</el-menu-item>
+          <el-menu-item index="4">裙子</el-menu-item>
+          <el-menu-item index="5">女鞋</el-menu-item>
+          <el-menu-item index="6">包包</el-menu-item>
+          <el-menu-item index="7">配饰</el-menu-item>
+          <el-menu-item index="8">美妆</el-menu-item>
+          <el-menu-item index="9">个护</el-menu-item>
+          <el-menu-item index="10">家具</el-menu-item>
+          <el-menu-item index="11">百货</el-menu-item>
+          <el-menu-item index="12">母婴</el-menu-item>
+          <el-menu-item index="13">食品</el-menu-item>
+      </el-menu>
+      </el-aside>
+      <el-main>
+        <list></list>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <script>
+  import List from "./componts/List";
+  import NavBar from "../../components/common/navbar/NavBar";
     export default {
-        name: "Category"
+        name: "Category",
+      data(){
+          return{
+            activeIndex: '1',
+          }
+      },
+      components:{
+          NavBar,
+        List
+      }
     }
 </script>
 
 <style scoped>
-
+  .nav{
+    background-color: var(--color-tint);
+    color: #ffffff;
+  }
+  .recommend{
+    margin-top: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content:space-around;
+  }
+  .recommend div{
+    width: 30%;
+  }
 </style>
---
+++ src/views/Category/componts/List.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+        <span style="font-weight: bold">推荐分类</span>
+        <div class="recommend">
+            <div style="text-align:center;">
+                <img src="../../.././assets/img/common/cloth.svg" style="width: 100%">
+                <span>女装馆</span>
+            </div>
+            <div style="text-align:center;">
+                <img src="../../.././assets/img/common/cloth.svg" style="width: 100%">
+                <span>通勤馆</span>
+            </div>
+            <div style="text-align:center;">
+                <img src="../../.././assets/img/common/cloth.svg" style="width: 100%">
+                <span>国风馆</span>
+            </div>
+            <div style="text-align:center;">
+                <img src="../../.././assets/img/common/cloth.svg" style="width: 100%">
+                <span>潮女馆</span>
+            </div>
+            <div style="text-align:center;">
+                <img src="../../.././assets/img/common/cloth.svg" style="width: 100%">
+                <span>潮流馆</span>
+            </div>
+            <div style="text-align:center;">
+                <img src="../../.././assets/img/common/cloth.svg" style="width: 100%">
+                <span>新品馆</span>
+            </div>
+        </div>
+        <el-divider></el-divider>
+        <span style="font-weight: bold">热卖选购</span>
+        <div class="recommend">
+            <div style="text-align:center;">
+                <img src="../../.././assets/img/common/cloth.svg" style="width: 100%">
+                <span>女装馆</span>
+            </div>
+            <div style="text-align:center;">
+                <img src="../../.././assets/img/common/cloth.svg" style="width: 100%">
+                <span>连衣裙</span>
+            </div>
+            <div style="text-align:center;">
+                <img src="../../.././assets/img/common/cloth.svg" style="width: 100%">
+                <span>T恤</span>
+            </div>
+            <div style="text-align:center;">
+                <img src="../../.././assets/img/common/cloth.svg" style="width: 100%">
+                <span>衬衫</span>
+            </div>
+            <div style="text-align:center;">
+                <img src="../../.././assets/img/common/cloth.svg" style="width: 100%">
+                <span>大码女装</span>
+            </div>
+            <div style="text-align:center;">
+                <img src="../../.././assets/img/common/cloth.svg" style="width: 100%">
+                <span>休闲裤</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "List"
+    }
+</script>
+
+<style scoped>
+    .recommend{
+        margin-top: 20px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content:space-around;
+    }
+    .recommend div{
+        width: 30%;
+    }
+    .recommend span{
+        color: #ff5777;
+    }
+</style>
